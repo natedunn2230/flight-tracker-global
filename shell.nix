770bcfac123fde453756a7d697028ac6eb07d4de
Nate <ndunn2230@gmail.com>
2023-08-12 23:31:53 -0400

    Removed sha check from shell for now and made install-deps executable

--- shell.nix
+++ shell.nix
@@ -1,6 +1,5 @@
 { pkgs ? import (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.05.tar.gz";
-    sha256 = "08sdywsz2kd9szd3pcfsm9vhpp370ldd7vndp0cia575f6yaibnm";
 }
 ) {} }:
 
