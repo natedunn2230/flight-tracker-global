c0092a1d696479c2c93717bd3dbb2bb8925d2ae6
Nate <ndunn2230@gmail.com>
2023-08-13 22:53:51 -0400

    Added ThreeJS and created sample scene

--- Readme.md
+++ Readme.md
@@ -6,5 +6,5 @@ Author: [Nathan Dunn](https://www.nathandunn.me)
 
 * Download Nix. See https://nixos.org/download.html.
 * Run the nix development shell, in the parent directory of the project: `nix-shell`
-* Install the dependencies: `./install-deps.sh`
+* Install the dependencies: `scripts/install-deps.sh`
 * Run the frontend, within *client/*: `npm start`
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import GlobalSceneComponent from './global-scene/global-scene.component';
+
+const routes: Routes = [
+  {'path': 'globe', component: GlobalSceneComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
